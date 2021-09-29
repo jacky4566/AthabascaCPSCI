52bef5d21341005b1622bfb9f5140f3a1a74f831
Jackson Wiebe <jacksonwiebe@me.com>
2021-09-28 22:08:10 -0600

    Captialization

--- COMP 268/Assign 4/Constants.java
+++ COMP 268/Assign 4/Constants.java
@@ -2,7 +2,7 @@ public class Constants {
     private Constants() {
     }
 
-    public static final String HELPFILE = "Help.txt";
-    public static final String WELCOMEFILE = "Welcome.txt";
+    public static final String HELPFILE = "HELP.txt";
+    public static final String WELCOMEFILE = "WELCOME.txt";
     public static final int CONSOLEWIDTH = 80;
 }
