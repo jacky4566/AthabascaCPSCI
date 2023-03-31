7831fb4eca39b4c093f5daa156422e6c8e0f4d33
Jackson Wiebe <jacksonwiebe@me.com>
2023-03-31 12:51:23 -0600

    Add Projects 1

---
+++ COMP 444/SIK_Circuit_1A-Blink/SIK_Circuit_1A-Blink.ino
@@ -0,0 +1,24 @@
+/*
+  Jackson Wiebe
+  31/03/2023
+  3519635
+*/
+
+void setup() {
+
+  pinMode(13, OUTPUT);      // Set pin 13 to output
+
+}
+
+
+void loop() {
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+
+  delay(2000);              // Wait for two seconds
+
+  digitalWrite(13, LOW);    // Turn off the LED
+
+  delay(2000);              // Wait for two seconds
+
+}
