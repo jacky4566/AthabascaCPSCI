6fac220eb5d48acfd2de4313c5d3dd84148512d3
Jackson Wiebe <jacksonwiebe@me.com>
2024-01-20 14:07:05 -0700

    remove completed courses

--- COMP 486/Mineral Mayhem/src/atoms/spriteSheetImageAtom.js
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-//Small Atom accessiable global to fetch the sprite sheet
- 
-export const spriteSheetImageAtom = atom({
-  key: "spriteSheetImageAtom",
-  default: null,
-});
